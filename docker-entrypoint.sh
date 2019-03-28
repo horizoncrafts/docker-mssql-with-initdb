fc1e11d84193608d6568f7c78b37f613c8971439
piotr.jodko <piotr.jodko@compactbi.com>
2019-03-28 10:21:00 +0100

    Setting the default value for MSSQL_SLEEP_TIME on a bit clearer way

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-( sleep ${MSSQL_SLEEP_TIME:-20} &&  \
+echo The server startup waiting time: ${MSSQL_SLEEP_TIME:=20}
+
+( sleep ${MSSQL_SLEEP_TIME} &&  \
   for f in /docker-entrypoint-initdb.d/*; do \
 	case "$f" in \
 		*.sql)    echo "$0: running $f"; /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -i "$f"; echo ;; \
